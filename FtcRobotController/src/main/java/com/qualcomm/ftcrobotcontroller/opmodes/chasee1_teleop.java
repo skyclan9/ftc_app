ea78bdbde1f52528d2d3e8455d8851878df17b9d
skyclan9 <skyclan9@aol.com>
2015-10-13 17:07:33 -0400

    updated opmode

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/chasee1_teleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/chasee1_teleop.java
@@ -1,24 +1,68 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 /**
  * Created by michaelgoldschlager on 10/13/15.
  */
 public class chasee1_teleop extends OpMode{
+
+    DcMotor leftback;
+    DcMotor leftfront;
+    DcMotor rightfront;
+    DcMotor rightback;
+    DcMotor climber;
+    DcMotorController climber_controller;
+    DcMotorController left_controller;
+    DcMotorController right_controller;
+    DcMotorController.DeviceMode devMode;
+    boolean climb = false;
+
+
     public chasee1_teleop (){
 
     }
 
     @Override
     public void init(){
-    
+        leftback = hardwareMap.dcMotor.get("leftback");
+        leftfront = hardwareMap.dcMotor.get("leftfront");
+        rightback = hardwareMap.dcMotor.get("rightback");
+        rightfront = hardwareMap.dcMotor.get("rightfront");
+        climber = hardwareMap.dcMotor.get("climber");
+        climber_controller = hardwareMap.dcMotorController.get("climber_controller");
+        left_controller = hardwareMap.dcMotorController.get("left_controller");
+        right_controller = hardwareMap.dcMotorController.get("right_controller");
+        devMode = DcMotorController.DeviceMode.WRITE_ONLY;
+
     }
 
     @Override
     public void loop(){
+        float left = -gamepad1.left_stick_y;
+        float right = -gamepad1.right_stick_y;
+        leftback.setPower(left);
+        leftfront.setPower(left);
+        rightback.setPower(right);
+        rightfront.setPower(right);
 
-        telemetry.addData("Text","***Robot Data***");
+       /* if (gamepad1.left_bumper == true && climb == false){
+            climb = true;
+            climber.setPower(100);
+        }
+        else if (gamepad1.left_bumper == true && climb == true){
+            climb = false;
+            climber.setPower(100);
+        }
+        */
+        if (gamepad1.left_bumper == true){
+            climber.setPower(100);
+        }
+        climber.setPower(0);
 
+        telemetry.addData("Text", "***Robot Data***");
+        telemetry.addData("Left Motor Power","Left Power:"+left);
+        telemetry.addData("Right Motor Power","Right Power:"+right);
 
     }
     @Override
