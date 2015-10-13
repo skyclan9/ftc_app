86f2879c8a31fd7bbd887f348897f6e8259f1882
skyclan9 <skyclan9@aol.com>
2015-10-13 16:25:35 -0400

    added teleop opmode

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/chasee1_teleop.java
@@ -0,0 +1,28 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by michaelgoldschlager on 10/13/15.
+ */
+public class chasee1_teleop extends OpMode{
+    public chasee1_teleop (){
+
+    }
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loop(){
+
+        telemetry.addData("Text","***Robot Data***");
+
+
+    }
+    @Override
+    public void stop(){
+
+    }
+}
