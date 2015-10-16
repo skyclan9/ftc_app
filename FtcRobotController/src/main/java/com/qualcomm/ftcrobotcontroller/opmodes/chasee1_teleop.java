efafc5897e192ba3a64314cc0a5468c07ae1a590
skyclan9 <skyclan9@aol.com>
2015-10-16 16:43:53 -0400

    added scaling

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/chasee1_teleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/chasee1_teleop.java
@@ -2,6 +2,8 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by michaelgoldschlager on 10/13/15.
  */
@@ -39,6 +41,10 @@ public class chasee1_teleop extends OpMode{
     public void loop(){
         float left = -gamepad1.left_stick_y;
         float right = gamepad1.right_stick_y;
+        right = Range.clip(right,-1,1);
+        left = Range.clip(left,-1,1);
+        right = (float)scaleInput(right);
+        left = (float)scaleInput(left);
         leftback.setPower(left);
         leftfront.setPower(left);
         rightback.setPower(right);
@@ -64,5 +70,33 @@ public class chasee1_teleop extends OpMode{
         telemetry.addData("Right Motor Power","Right Power:"+right);
         telemetry.addData("Climber", "Climber:"+climb_pow);
 
+    }
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
     }
 }
